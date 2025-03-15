41c18d8a8a61050858983b028c763b6bde8db309
Nikita_Voronov <nekitos199@gmail.com>
2025-03-15 19:53:21 +0000

    update

--- result_hw/dags/generate_mongo_data.py
+++ result_hw/dags/generate_mongo_data.py
@@ -1,36 +1,61 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from pymongo import MongoClient
-from faker import Faker
-from lib import default_args
+from datetime import datetime, timedelta
+import random
+from lib import default_args, mongo_client
+import uuid
 
-def generate_mongo_data():
-    fake = Faker()
-    client = MongoClient("mongodb://root:example@mongodb:27017/")
-    db = client["mydatabase"]
-    
-    # Генерация UserSessions
-    db.UserSessions.insert_one({
-        "session_id": fake.uuid4(),
-        "user_id": fake.random_int(1, 1000),
-        "start_time": fake.date_time_this_month(),
-        "end_time": fake.date_time_this_month(),
-        "pages_visited": [fake.uri() for _ in range(5)],
-        "device": fake.user_agent(),
-        "actions": [fake.word() for _ in range(3)]
-    })
-    
-    # Аналогично для других коллекций...
+
+COUNT_ROWS_TO_GENERATE = 1000
+
+
+def generate_user_session() -> dict:
+    return {
+        "session_id": str(random.randint(10**3, 10**6)),
+        "user_id": random.randint(10**1, 10**3),
+        "start_time": datetime.now(),
+        "end_time": datetime.now() + timedelta(minutes=random.randint(1, 60)),
+        "pages_visited": ["/home", "/product"],
+        "device": {"type": "mobile", "os": "Android"},
+        "actions": ["click", "scroll"],
+    }
+
+
+def generate_support_ticket() -> dict:
+    return {
+        "ticket_id": str(object=uuid.uuid4()),
+        "user_id": random.randint(10**1, 10**3),
+        "status": random.choice(["open", "closed", "pending"]),
+        "issue_type": random.choice(["technical", "billing", "general"]),
+        "created_at": datetime.now(),
+        "updated_at": datetime.now(),
+    }
+
+
+def generate_data() -> None:
+    collection = mongo_client["raw_data"]["user_sessions"]
+    support_tickets = mongo_client["raw_data"]["support_tickets"]
+
+    documents: list[dict] = [
+        generate_user_session() for _ in range(COUNT_ROWS_TO_GENERATE)
+    ]
+
+    tickets: list[dict] = [
+        generate_support_ticket() for _ in range(COUNT_ROWS_TO_GENERATE)
+    ]
+
+    collection.insert_many(documents)
+    support_tickets.insert_many(tickets)
+
 
 dag = DAG(
-    'generate_mongo_data',
+    "generate_mongo_data",
     default_args=default_args,
-    schedule_interval='*/10 * * * *',
-    catchup=False
+    schedule_interval="*/10 * * * *",
+    catchup=False,
 )
 
 generate_task = PythonOperator(
-    task_id='generate_mongo_data',
-    python_callable=generate_mongo_data,
-    dag=dag
+    task_id="generate_mongo_data", python_callable=generate_data, dag=dag
 )
+
--- result_hw/dags/mongo_to_postgres.py
+++ result_hw/dags/mongo_to_postgres.py
@@ -1,55 +1,89 @@
-from datetime import datetime
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from pymongo import MongoClient
-import psycopg2
+from datetime import datetime, timedelta
+import json
+from lib import default_args, mongo_client, pg_conn
+import logging
 
-from lib import default_args
 
-def mongo_to_postgres():
-    # Подключение к MongoDB
-    mongo_client = MongoClient("mongodb://root:example@mongodb:27017/")
-    db = mongo_client["mydatabase"]
-    
-    # Подключение к PostgreSQL
-    pg_conn = psycopg2.connect(
-        dbname='airflow',
-        user='airflow',
-        password='airflow',
-        host='postgres'
-    )
-    cursor = pg_conn.cursor()
-    
-    # Трансформация и загрузка данных
-    # Пример для UserSessions
-    sessions = db.UserSessions.find()
-    for session in sessions:
-        cursor.execute("""
-            INSERT INTO user_sessions 
-            (session_id, user_id, start_time, end_time, device)
-            VALUES (%s, %s, %s, %s, %s)
-            ON CONFLICT (session_id) DO NOTHING
-        """, (
-            session['session_id'],
-            session['user_id'],
-            session['start_time'],
-            session['end_time'],
-            session['device']
-        ))
-    
-    pg_conn.commit()
-    cursor.close()
-    pg_conn.close()
+def migrate_user_sessions() -> None:
+    user_sessions = mongo_client["raw_data"]["user_sessions"]
+    support_tickets = mongo_client["raw_data"]["support_tickets"]
 
-dag = DAG(
-    'mongo_to_postgres',
+    """
+        CREATE TABLE IF NOT EXISTS user_sessions (
+            session_id TEXT PRIMARY KEY,
+            user_id INT,
+            start_time TIMESTAMP,
+            end_time TIMESTAMP,
+            pages_visited JSONB,
+            device JSONB,
+            actions JSONB
+        )
+    """
+
+    """
+        CREATE TABLE IF NOT EXISTS replicated_data.support_tickets (
+                ticket_id TEXT PRIMARY KEY,
+                user_id INT,
+                status TEXT,
+                issue_type TEXT,
+                created_at TIMESTAMP,
+                updated_at TIMESTAMP
+            )
+    """
+
+    last_hour = datetime.now() - timedelta(hours=1)
+    sessions = user_sessions.find({"start_time": {"$gte": last_hour}})
+    tickets = support_tickets.find({"updated_at": {"$gte": last_hour}})
+
+    with pg_conn.cursor() as cursor:
+        for session in sessions:
+            cursor.execute(
+                query="""
+                INSERT INTO replicated_data.user_sessions 
+                VALUES (%s, %s, %s, %s, %s, %s, %s)
+                ON CONFLICT (session_id) DO NOTHING
+            """,
+                vars=(
+                    session["session_id"],
+                    session["user_id"],
+                    session["start_time"],
+                    session["end_time"],
+                    json.dumps(session["pages_visited"]),
+                    json.dumps(session["device"]),
+                    json.dumps(session["actions"]),
+                ),
+            )
+
+        for ticket in tickets:
+            cursor.execute(
+                query="""
+                INSERT INTO replicated_data.support_tickets 
+                VALUES (%s, %s, %s, %s, %s, %s)
+                ON CONFLICT (ticket_id) DO NOTHING
+            """,
+                vars=(
+                    ticket["ticket_id"],
+                    ticket["user_id"],
+                    ticket["status"],
+                    ticket["issue_type"],
+                    ticket["created_at"],
+                    ticket["updated_at"],
+                ),
+            )
+            
+        pg_conn.commit()
+        pg_conn.close()
+
+
+with DAG(
+    "mongo_to_postgres",
     default_args=default_args,
-    schedule_interval='@hourly',
-    catchup=False
-)
+    schedule_interval="@hourly",
+    start_date=datetime(2024, 1, 1),
+) as dag:
+    migrate_task = PythonOperator(
+        task_id="migrate_user_sessions", python_callable=migrate_user_sessions
+    )
 
-transfer_task = PythonOperator(
-    task_id='transfer_mongo_to_postgres',
-    python_callable=mongo_to_postgres,
-    dag=dag
-)
